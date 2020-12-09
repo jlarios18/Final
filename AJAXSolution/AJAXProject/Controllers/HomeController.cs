37ea7f413bc7aa41223869baa64cbb472034d261
Jimmy Larios <jimmylarios96@gmail.com>
2020-12-08 22:27:06 -0800

    Trying ajax online video example

---
+++ AJAXSolution/AJAXProject/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AJAXProject.Models;
+
+namespace AJAXProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public ActionResult Math(int nr1, int nr2)
+        {
+            var result = nr1 + nr2;
+            var data = new { status = "ok", results = result }; //dynamic object
+            return Json(data);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AJAXSolution/AJAXProject/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿
+$("#calculate").click(function () {
+    let nr1 = $("#nr1").val();
+    let nr2 = $("#nr2").val();
+    $.ajax({
+        //url: '@Url.Action("Math", "Home")?nr1=' + nr1 + '&nr2' + nr2,
+        url: '@Url.Action("Math", "Home")?nr1=' + nr1 + '&nr2' + nr2,
+        success: function (data) {
+            if (data.status == "ok") {
+                $("#results").html(data.result); //data.result a dynamic object too
+            }
+        }
+    });
+});
